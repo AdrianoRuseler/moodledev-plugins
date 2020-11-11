3a49b2efc15fa8285e5e193c5dcd328b3118a036
Adriano Ruseler <ruseler@utfpr.edu.br>
2020-11-11 15:04:14 -0300

    Create createrepo.bat

---
+++ createrepo.bat
@@ -0,0 +1,16 @@
+
+mkdir moodle
+cd moodle
+
+git submodule add -b master https://github.com/mudrd8mz/moodle-tool_pluginskel.git admin/tool/pluginskel
+
+git submodule add -b master https://github.com/moodlehq/moodle-local_codechecker.git local/codechecker
+
+git submodule add -b master https://github.com/moodlehq/moodle-local_moodlecheck.git local/moodlecheck
+
+pause
+
+
+
+
+
