2647928502530a0ebf6a709a798536e42e600c8d
Adriano Ruseler <ruseler@inep.ufsc.br>
2021-10-21 13:53:34 -0300

    Update DeleteApacheLocalSite.sh

--- scripts/DeleteApacheLocalSite.sh
+++ scripts/DeleteApacheLocalSite.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
 # Set web server (apache)
-LOCALSITENAME="devtest.local"
-LOCALSITEFOLDER="devtest"
+# export LOCALSITENAME="devtest.local"
+# export LOCALSITEFOLDER="devtest"
+
+
+if [[ ! -v LOCALSITENAME ]]; then
+    echo "LOCALSITENAME is not set"
+        exit 1
+elif [[ -z "$LOCALSITENAME" ]]; then
+    echo "LOCALSITENAME is set to the empty string"
+        exit 1
+else
+    echo "LOCALSITENAME has the value: $LOCALSITENAME"
+fi
+
+if [[ ! -v LOCALSITEFOLDER ]]; then
+    echo "LOCALSITEFOLDER is not set"
+        exit 1
+elif [[ -z "$LOCALSITEFOLDER" ]]; then
+    echo "LOCALSITEFOLDER is set to the empty string"
+        exit 1
+else
+    echo "LOCALSITEFOLDER has the value: $LOCALSITEFOLDER"
+fi
+
 
 # systemctl status apache2.service --no-pager --lines=2
 
@@ -19,8 +41,6 @@ rm -rf /var/www/html/${LOCALSITEFOLDER}
 
 systemctl status apache2.service --no-pager --lines=2
 
-
 IP4STR=$(ip -4 addr show enp0s3 | grep -oP "(?<=inet ).*(?=/)")
 echo ""
 echo "Remove $IP4STR $LOCALSITENAME from %WINDIR%\System32\drivers\etc\hosts "
-
