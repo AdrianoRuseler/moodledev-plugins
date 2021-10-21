e9e0134b28e0d508a7a57b6cd5ab1660e5cb845a
Adriano Ruseler <ruseler@inep.ufsc.br>
2021-10-21 12:55:39 -0300

    Update DeleteApacheLocalSite.sh

--- scripts/DeleteApacheLocalSite.sh
+++ scripts/DeleteApacheLocalSite.sh
@@ -4,6 +4,8 @@
 LOCALSITENAME="devtest.local"
 LOCALSITEFOLDER="devtest"
 
+service apache2 status --no-pager
+
 # Enable site
 sudo a2dissite ${LOCALSITENAME}-ssl.conf
 sudo systemctl reload apache2
@@ -14,4 +16,11 @@ rm /etc/apache2/sites-available/${LOCALSITENAME}-ssl.conf /etc/ssl/certs/${LOCAL
 # Remove folder
 rm -rf /var/www/html/${LOCALSITEFOLDER}
 
+
+service apache2 status --no-pager
+
+
+IP4STR=$(ip -4 addr show enp0s3 | grep -oP "(?<=inet ).*(?=/)")
+echo ""
+echo "Remove $IP4STR $LOCALSITENAME from %WINDIR%\System32\drivers\etc\hosts "
 
