758ce2c4070629074960df3af6ebde0a0ad20a19
Adriano Ruseler <ruseler@inep.ufsc.br>
2021-10-23 11:17:47 -0300

    Create defaults-dist.php

---
+++ config/defaults-dist.php
@@ -0,0 +1,23 @@
+<?php
+$defaults['moodle']['summary'] = 'Moodle Site! '; // for core settings
+$defaults['moodle']['custommenuitems'] = 'Tema
+-Boost | ?theme=boost
+-Classic | ?theme=classic
+Criação de curso | /course/request.php
+';
+
+$defaults['moodle']['timezone'] = 'America/Sao_Paulo';
+$defaults['moodle']['defaultcity'] = 'Curitiba';
+
+$defaults['moodle']['pathtophp'] = '/usr/bin/php';
+$defaults['moodle']['pathtodu'] = '/usr/bin/du';
+$defaults['moodle']['aspellpath'] = '/usr/bin/aspell';
+$defaults['moodle']['pathtogs'] = '/usr/bin/gs';
+$defaults['moodle']['pathtodot'] = '/usr/bin/dot';
+$defaults['moodle']['pathtopdftoppm'] = '/usr/bin/pdftoppm';
+$defaults['moodle']['pathtopython'] = '/usr/bin/python3';
+
+$defaults['moodle']['auth_instructions'] = 'Usuário: admin
+Senha: myadmpass';
+$defaults['moodle']['forcelogin'] = 1;
+
