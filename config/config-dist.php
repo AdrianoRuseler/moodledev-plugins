b44e9cee1dbf4efceaa1c33b692e9d73848f1558
Adriano Ruseler <ruseler@inep.ufsc.br>
2021-10-25 11:15:36 -0300

    Update config-dist.php

--- config/config-dist.php
+++ config/config-dist.php
@@ -21,8 +21,21 @@ $CFG->dboptions = array (
 $CFG->wwwroot   = 'mysiteurl';
 $CFG->dataroot  = '/var/www/data/mydatafolder';
 $CFG->admin     = 'admin';
-
 $CFG->directorypermissions = 0777;
+
+// Use the following flag to completely disable the installation of plugins
+// (new plugins, available updates and missing dependencies) and related
+// features (such as cancelling the plugin installation or upgrade) via the
+// server administration web interface.
+$CFG->disableupdateautodeploy = true;
+// Disabling update notifications
+$CFG->disableupdatenotifications = true;
+
+// Some administration options allow setting the path to executable files. This can
+// potentially cause a security risk. Set this option to true to disable editing
+// those config settings via the web. They will need to be set explicitly in the
+// config.php file
+// $CFG->preventexecpath = true;
 
 require_once(__DIR__ . '/lib/setup.php');
 
