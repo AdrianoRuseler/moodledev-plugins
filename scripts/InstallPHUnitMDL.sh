7c8decac59b629bb8abb096fb0f0137531a9d5ca
Adriano Ruseler <ruseler@inep.ufsc.br>
2021-10-26 07:58:33 -0300

    Update InstallPHUnitMDL.sh

--- scripts/InstallPHUnitMDL.sh
+++ scripts/InstallPHUnitMDL.sh
@@ -16,6 +16,17 @@ else
     echo "LOCALSITENAME has the value: $LOCALSITENAME"	
 fi
 
+# If /root/.my.cnf exists then it won't ask for root password
+if [ -f /root/.my.cnf ]; then
+   echo "/root/.my.cnf exists"
+# If /root/.my.cnf doesn't exist then it'll ask for password   
+else
+	if [[ ! -v ADMDBUSER ]] || [[ -z "$ADMDBUSER" ]] || [[ ! -v ADMDBPASS ]] || [[ -z "$ADMDBPASS" ]]; then
+		echo "ADMDBUSER or ADMDBPASS is not set or is set to the empty string!"
+		exit 1
+	fi
+fi
+
 ENVFILE='.'${LOCALSITENAME}'.env'
 if [ -f $ENVFILE ]; then
 	# Load Environment Variables
@@ -35,21 +46,14 @@ else
     echo "LOCALSITEURL has the value: $LOCALSITEURL"
 fi
 
-# Verify for MDLHOME
-if [[ ! -v MDLHOME ]] || [[ -z "$MDLHOME" ]]; then
-    echo "MDLHOME is not set or is set to the empty string!"
+# Verify for MDLHOME and folder
+if [[ ! -v MDLHOME ]] || [[ -z "$MDLHOME" ]] || [[ -d "$MDLHOME" ]]; then
+    echo "MDLHOME is not set or is set to the empty string or NOT exists on your filesystem!"
     exit 1
 else
     echo "MDLHOME has the value: $MDLHOME"	
 fi
 
-# Verify if folder exists
-if [[ -d "$MDLHOME" ]]; then
-	echo "$MDLHOME exists on your filesystem."
-else
-    echo "$MDLHOME NOT exists on your filesystem."
-	exit 1
-fi
 
 # Verify for config.php file
 if [[ -f "$MDLHOME/config.php" ]]; then
@@ -64,12 +68,31 @@ datastr=$(date) # Generates datastr
 echo "" >> $ENVFILE
 echo "# ----- $datastr -----" >> $ENVFILE
 
+echo ""
+echo "##---------------------- GENERATES PHPUNIT DB -------------------------##"
+echo ""
+
+PHPUNITDBNAME=$(pwgen 10 -s1vA0) # Generates ramdon user name
+PHPUNITDBUSER=$PHPUNITDBNAME # Use same generated ramdon user name
+PHPUNITDBPASS=$(pwgen -s 14 1) # Generates ramdon password for db user
+# DBPASS="$(openssl rand -base64 12)"
+echo "PHPUNIT DB Name: $PHPUNITDBNAME"
+echo "PHPUNIT DB User: $PHPUNITDBUSER"
+echo "PHPUNIT DB Pass: $PHPUNITDBPASS"
+echo ""
+
+# Save Environment Variables
+echo "" >> $ENVFILE
+echo "# DataBase credentials" >> $ENVFILE
+echo "PHPUNITDBNAME=\"$PHPUNITDBNAME\"" >> $ENVFILE
+echo "PHPUNITDBUSER=\"$PHPUNITDBUSER\"" >> $ENVFILE
+echo "PHPUNITDBPASS=\"$PHPUNITDBPASS\"" >> $ENVFILE
+
 # Verify for PHPUNITDATA
 if [[ ! -v PHPUNITDATA ]] || [[ -z "$PHPUNITDATA" ]]; then
     echo "PHPUNITDATA is not set or is set to the empty string!"
 	PHPUNITDATA=/var/www/data/phpunit/${LOCALSITENAME}
 	mkdir $PHPUNITDATA
-	chown www-data:www-data -R $PHPUNITDATA
 	echo "PHPUNITDATA=\"$PHPUNITDATA\"" >> $ENVFILE
 else
     echo "PHPUNITDATA has the value: $PHPUNITDATA"	
@@ -90,6 +113,15 @@ else
     echo "PHPUNITPREFIX has the value: $PHPUNITPREFIX"	
 fi
 
+sed -i '/require_once*/i $CFG->phpunit_dbtype    = \x27mariadb\x27;' $MDLCONFIGFILE # Single quote \x27
+sed -i '/require_once*/i $CFG->phpunit_dblibrary = \x27native\x27;' $MDLCONFIGFILE # Single quote \x27
+sed -i '/require_once*/i $CFG->phpunit_dbhost    = \x27127.0.0.1\x27;' $MDLCONFIGFILE # Single quote \x27
+sed -i '/require_once*/i $CFG->phpunit_dbname = \x27'$PHPUNITDBNAME'\x27;' $MDLCONFIGFILE # Single quote \x27
+sed -i '/require_once*/i $CFG->phpunit_dbuser = \x27'$PHPUNITDBUSER'\x27;' $MDLCONFIGFILE # Single quote \x27
+sed -i '/require_once*/i $CFG->phpunit_dbpass = \x27'$PHPUNITDBUSER'\x27;\n' $PHPUNITDBPASS # Single quote \x27
+
+sed -i '/require_once*/i $CFG->phpunit_dataroot = \x27'$PHPUNITDATA'\x27;' $MDLCONFIGFILE # Single quote \x27
+sed -i '/require_once*/i $CFG->phpunit_prefix = \x27'$PHPUNITPREFIX'\x27;\n' $MDLCONFIGFILE # Single quote \x27
 
 sudo -u www-data composer -V
 #php composer.phar install
@@ -99,8 +131,6 @@ sudo -u www-data composer -V
 cd $MDLHOME
 sudo -u www-data composer install
 
-sed -i '/require_once*/i $CFG->phpunit_dataroot = \x27'$PHPUNITDATA'\x27;' $MDLCONFIGFILE # Single quote \x27
-sed -i '/require_once*/i $CFG->phpunit_prefix = \x27'$PHPUNITPREFIX'\x27;\n' $MDLCONFIGFILE # Single quote \x27
 
 echo "Initialise the test environment..."
 sudo -u www-data /usr/bin/php $MDLHOME/admin/tool/phpunit/cli/init.php
@@ -113,3 +143,9 @@ echo "##------------ $ENVFILE -----------------##"
 
 #To execute all test suites from main configuration file execute the
 # vendor/bin/phpunit
+
+# cat $MDLHOME/phpunit.xml
+# vendor/bin/phpunit --testsuite core_phpunit_testsuite
+
+# https://docs.moodle.org/dev/PHPUnit
+
