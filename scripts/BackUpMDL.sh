959950e83df80456379f6eea2925f97f5d135a0a
Adriano Ruseler <ruseler@inep.ufsc.br>
2021-10-31 23:21:28 -0300

    Create BackUpMDL.sh

---
+++ scripts/BackUpMDL.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+# Load Environment Variables
+if [ -f .env ]; then	
+	export $(grep -v '^#' .env | xargs)
+fi
+
+# Verify for LOCALSITENAME
+if [[ ! -v LOCALSITENAME ]] || [[ -z "$LOCALSITENAME" ]]; then
+    echo "LOCALSITENAME is not set or is set to the empty string!"
+	echo "Choose site to use:"
+	ls /etc/apache2/sites-enabled/
+	echo "export LOCALSITENAME=teste"
+else
+    echo "LOCALSITENAME has the value: $LOCALSITENAME"	
+fi
+
+ENVFILE='.'${LOCALSITENAME}'.env'
+if [ -f $ENVFILE ]; then
+	# Load Environment Variables
+	export $(grep -v '^#' $ENVFILE | xargs)
+	echo ""
+	echo "##------------ $ENVFILE -----------------##"
+	cat $ENVFILE
+	echo "##------------ $ENVFILE -----------------##"
+	echo ""
+#	rm $ENVFILE
+fi
+
+if [[ ! -v LOCALSITEURL ]] || [[ -z "$LOCALSITEURL" ]]; then
+    echo "LOCALSITEURL is not set or is set to the empty string!"
+	LOCALSITEURL=${LOCALSITENAME}'.local' # Generates ramdon site name
+else
+    echo "LOCALSITEURL has the value: $LOCALSITEURL"
+fi
+
+datastr=$(date) # Generates datastr
+echo "" >> $ENVFILE
+echo "# ----- $datastr -----" >> $ENVFILE
+
+# Verify for MDLHOME and MDLDATA
+if [[ ! -v MDLHOME ]] || [[ -z "$MDLHOME" ]] || [[ ! -v MDLDATA ]] || [[ -z "$MDLDATA" ]]; then
+    echo "MDLHOME or MDLDATA is not set or is set to the empty string!"
+    exit 1
+else
+    echo "MDLHOME has the value: $MDLHOME"	
+	echo "MDLDATA has the value: $MDLDATA"
+fi
+
+# Verify if folder exists
+if [[ -d "$MDLHOME" ]] && [[ -d "$MDLDATA" ]]; then
+	echo "$MDLHOME and $MDLDATA exists on your filesystem."
+else
+    echo "$MDLHOME or $MDLDATA NOT exists on your filesystem."
+	exit 1
+fi
+
+# Verify for DB Credentials
+if [[ ! -v DBNAME ]] || [[ -z "$DBNAME" ]]; then
+    echo "DBNAME is not set or is set to the empty string!"
+    exit 1
+else
+    echo "DBNAME has the value: $DBNAME"	
+fi
+
+BKPDIR="/home/ubuntu/backups/"$LOCALSITENAME  # moodle backup folder
+mkdir $BKPDIR
+DBBKP=$BKPDIR"/db/" # moodle database backup folder
+mkdir $DBBKP
+DATABKP=$BKPDIR"/data/"  # moodle data backup folder
+mkdir $DATABKP
+HTMLBKP=$BKPDIR"/html/"  # moodle html backup folder
+mkdir $HTMLBKP
+
+filename=$(date +\%Y-\%m-\%d-\%H.\%M)
+
+echo "Kill all user sessions..."
+sudo -u www-data /usr/bin/php $MDLHOME/admin/cli/kill_all_sessions.php
+
+echo "Activating Moodle Maintenance Mode in..."
+sudo -u www-data /usr/bin/php $MDLHOME/admin/cli/maintenance.php --enable
+
+
+# make database backup
+
+# mysqldump integration | gzip > integration.sql.gz
+
+
+mysqldump $DBNAME | gzip > $DBBKP$filename.sql.gz
+md5sum $DBBKP$filename.sql.gz > $DBBKP$filename.sql.gz.md5
+md5sum -c $DBBKP$filename.sql.gz.md5
+
+ls -lh $DBBKP
+
+# Backup the files using tar.
+tar -czf $DATABKP$filename.tar.gz $MOODLE_DATA
+md5sum $DATABKP$filename.tar.gz > $DATABKP$filename.tar.gz.md5
+md5sum -c $DATABKP$filename.tar.gz.md5
+
+ls -lh $DATABKP
+
+tar -czf $HTMLBKP$filename.tar.gz $MDLHOME
+md5sum $HTMLBKP$filename.tar.gz > $HTMLBKP$filename.tar.gz.md5
+md5sum -c $HTMLBKP$filename.tar.gz.md5
+
+ls -lh $HTMLBKP
+
+echo "disable the maintenance mode..."
+sudo -u www-data /usr/bin/php $MDLHOME/admin/cli/maintenance.php --disable
+
+
+
+cd ~
+echo ""
+echo "##------------ $ENVFILE -----------------##"
+cat $ENVFILE
+echo "##------------ $ENVFILE -----------------##"
+
