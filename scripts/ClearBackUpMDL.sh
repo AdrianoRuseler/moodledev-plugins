ed80a4230fcc0158d20440ee9eea35fe10e2896d
Adriano Ruseler <ruseler@inep.ufsc.br>
2021-10-31 23:38:16 -0300

    Update ClearBackUpMDL.sh

--- scripts/ClearBackUpMDL.sh
+++ scripts/ClearBackUpMDL.sh
@@ -15,54 +15,6 @@ else
     echo "LOCALSITENAME has the value: $LOCALSITENAME"	
 fi
 
-ENVFILE='.'${LOCALSITENAME}'.env'
-if [ -f $ENVFILE ]; then
-	# Load Environment Variables
-	export $(grep -v '^#' $ENVFILE | xargs)
-	echo ""
-	echo "##------------ $ENVFILE -----------------##"
-	cat $ENVFILE
-	echo "##------------ $ENVFILE -----------------##"
-	echo ""
-#	rm $ENVFILE
-fi
-
-if [[ ! -v LOCALSITEURL ]] || [[ -z "$LOCALSITEURL" ]]; then
-    echo "LOCALSITEURL is not set or is set to the empty string!"
-	LOCALSITEURL=${LOCALSITENAME}'.local' # Generates ramdon site name
-else
-    echo "LOCALSITEURL has the value: $LOCALSITEURL"
-fi
-
-datastr=$(date) # Generates datastr
-echo "" >> $ENVFILE
-echo "# ----- $datastr -----" >> $ENVFILE
-
-# Verify for MDLHOME and MDLDATA
-if [[ ! -v MDLHOME ]] || [[ -z "$MDLHOME" ]] || [[ ! -v MDLDATA ]] || [[ -z "$MDLDATA" ]]; then
-    echo "MDLHOME or MDLDATA is not set or is set to the empty string!"
-    exit 1
-else
-    echo "MDLHOME has the value: $MDLHOME"	
-	echo "MDLDATA has the value: $MDLDATA"
-fi
-
-# Verify if folder exists
-if [[ -d "$MDLHOME" ]] && [[ -d "$MDLDATA" ]]; then
-	echo "$MDLHOME and $MDLDATA exists on your filesystem."
-else
-    echo "$MDLHOME or $MDLDATA NOT exists on your filesystem."
-	exit 1
-fi
-
-# Verify for DB Credentials
-if [[ ! -v DBNAME ]] || [[ -z "$DBNAME" ]]; then
-    echo "DBNAME is not set or is set to the empty string!"
-    exit 1
-else
-    echo "DBNAME has the value: $DBNAME"	
-fi
-
 BKPDIR="/home/ubuntu/backups/"$LOCALSITENAME  # moodle backup folder
 DBBKP=$BKPDIR"/db/" # moodle database backup folder
 DATABKP=$BKPDIR"/data/"  # moodle data backup folder
