6753b7c5b9aed6ccee3dac27f82f65241ebe0818
Adriano Ruseler <ruseler@inep.ufsc.br>
2021-11-01 15:07:56 -0300

    Update ClearBackUpMDL.sh

--- scripts/ClearBackUpMDL.sh
+++ scripts/ClearBackUpMDL.sh
@@ -20,21 +20,45 @@ DBBKP=$BKPDIR"/db/" # moodle database backup folder
 DATABKP=$BKPDIR"/data/"  # moodle data backup folder
 HTMLBKP=$BKPDIR"/html/"  # moodle html backup folder
 
-cd $DBBKP
+# Verify for BKPNAME -- Not set on .env file
+if [[ ! -v BKPNAME ]] || [[ -z "$BKPNAME" ]]; then
+    echo "BKPNAME is not set or is set to the empty string!"
+    # Remove All
+	cd $DBBKP
 
-ls -lh $DBBKP
-rm -rf *.gz
-rm -rf *.gz.md5
+	ls -lh $DBBKP
+	rm -rf *.gz
+	rm -rf *.gz.md5
 
-cd $DATABKP
+	cd $DATABKP
 
-ls -lh $DATABKP
-rm -rf *.gz
-rm -rf *.gz.md5
+	ls -lh $DATABKP
+	rm -rf *.gz
+	rm -rf *.gz.md5
 
-cd $HTMLBKP
+	cd $HTMLBKP
 
-ls -lh $HTMLBKP
-rm -rf *.gz
-rm -rf *.gz.md5
+	ls -lh $HTMLBKP
+	rm -rf *.gz
+	rm -rf *.gz.md5
+	
+else
+    echo "BKPNAME has the value: $BKPNAME"	
+	# Remove selected
+	rm -rf $DBBKP$BKPNAME.sql
+	rm -rf $DBBKP$BKPNAME.sql.md5	
+	
+	rm -rf $DBBKP$BKPNAME.tar.gz
+	rm -rf $DBBKP$BKPNAME.tar.gz.md5
+
+	rm -rf $DATABKP$BKPNAME.tar.gz
+	rm -rf $DATABKP$BKPNAME.tar.gz.md5
+
+	rm -rf $HTMLBKP$BKPNAME.tar.gz
+	rm -rf $HTMLBKP$BKPNAME.tar.gz.md5
+fi
+
+
+
+
 
